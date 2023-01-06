6c468d193e9862e8f4760ef921e46c074c676e4d
luamz <luamsilva@id.uff.br>
2023-01-06 20:00:29 -0300

    Removing outdated attributions

--- src/h2_load_repository.py
+++ src/h2_load_repository.py
@@ -132,30 +132,10 @@ def load_repository(session, domain, repo, check_repo_only=True, branch=None,
 
     vprint(1, "Finding files")
 
-
-    # setups, requirements, pipfiles, pipfile_locks = find_files_in_path(
-    #     full_dir, [
-    #         "setup.py", "requirements.txt", "Pipfile", "Pipfile.lock"
-    #     ]
-    # )
-
     repository = Repository(
         domain=domain, repository=repo,
         hash_dir1=part, hash_dir2=end,
         commit=commit,
-
-        # notebooks_count=len(notebooks),
-        # setups_count=len(setups),
-        # requirements_count=len(requirements),
-        # pipfiles_count=len(pipfiles),
-        # pipfile_locks_count=len(pipfile_locks),
-        #
-        # # notebooks=join_paths(notebooks),
-        # setups=join_paths(setups),
-        # requirements=join_paths(requirements),
-        # pipfiles=join_paths(pipfiles),
-        # pipfile_locks=join_paths(pipfile_locks),
-
         processed=consts.R_OK,
     )
     session.add(repository)
